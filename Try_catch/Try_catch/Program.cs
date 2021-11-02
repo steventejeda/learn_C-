4f6a8d1d5db299d1d7b728516c64a30c33275b4b
steventejeda <steventejeda@gmail.com>
2021-11-02 14:26:15 -0400

    try & catch, learning operators

---
+++ Try_catch/Try_catch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TryAndCatchC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string userInput = Console.ReadLine();
+            
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+                Console.WriteLine($"The number you entered is {userInputAsInt}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter numbers only.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: The number you entered was too long");
+            }
+            finally
+            {
+                Console.WriteLine("Success!");
+            }
+
+
+
+        }
+    }
+}
