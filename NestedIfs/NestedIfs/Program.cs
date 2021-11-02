16fecb17c938ded3de5257a7b8a79b79bc733178
steventejeda <steventejeda@gmail.com>
2021-11-02 17:28:42 -0400

    Nested If statements/User login system

--- NestedIfs/NestedIfs/Program.cs
+++ NestedIfs/NestedIfs/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Nest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Please enter your username");
+            userName = Console.ReadLine();
+
+            if (isRegistered)
+            {
+                Console.WriteLine("Hi there, registered user");
+                if(userName != "")
+                {
+                    Console.WriteLine($"Hi there, {userName}");
+                    if (userName.Equals("admin"))
+                    {
+                        Console.WriteLine("Hi there, Admin");
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }   
+}
