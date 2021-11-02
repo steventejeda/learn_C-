cfac04ca50d772946f71532f2e079de018b9ab8d
steventejeda <steventejeda@gmail.com>
2021-11-02 16:32:49 -0400

    Operaters and if statements

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OperatorsC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the temperature like?");
+            string temperature = Console.ReadLine();
+            int numTemp;
+            int number;
+            bool userEnteredANumber = int.TryParse(temperature, out number);  
+
+
+            if(int.TryParse(temperature, out number))
+            {
+                numTemp = number;
+            }
+            else
+            {
+                numTemp = 0; 
+            }
+
+            if(numTemp < 40)
+                {
+                    Console.WriteLine("Take the coat");
+                } else if(numTemp == 60)
+                {
+                    Console.WriteLine("Pants and pull over will be fine");
+                } else if(numTemp > 90){
+                    Console.WriteLine("It's super hot!");
+                } else
+                {
+                    
+                    Console.WriteLine("Invalid entry");
+                }
+        }
+    }
+}
