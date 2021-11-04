d0157a5421aedc053a493bca38c38bd2dd0f75e4
steventejeda <steventejeda@gmail.com>
2021-11-04 16:23:53 -0400

    intro to OOP

---
+++ switch_statement/switch_statement/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Human
+    {
+        //member variable
+        public string firstName;
+
+        //member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"Hi, I'm {firstName}");
+        }
+    }
+}
---
+++ switch_statement/switch_statement/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Create an instance of my class
+        Human steven = new Human();
+            //New instance 
+            steven.firstName = "Steven";
+
+            steven.IntroduceMyself();
+
+            Console.Read();     
+        }
+    }
+}
