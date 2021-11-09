a10e8a00bbb796069ac95e3f639ca0f7adbd2fcf
steventejeda <steventejeda@gmail.com>
2021-11-08 21:00:50 -0500

    OOP properties

---
+++ properties/properties/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+     class Box
+    {
+        //member varibles
+        public int length;
+        public int height;
+        public int width;
+        public int volume;
+
+        public void DisplayInfo()
+        {
+            volume = length * width * height;
+            Console.WriteLine($"length is {length}, height is {height}, and width is {width} so the volume is {volume}");
+        }
+    }
+}
---
+++ properties/properties/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            box.length = 1;
+            box.width = 2; 
+            box.height = 3;
+
+            box.DisplayInfo();
+            Console.Read();
+        }
+    }
+}
+
+    
+    
+    
+  
